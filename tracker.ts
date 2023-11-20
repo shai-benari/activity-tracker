f3c05429a925bb496c8d427e2c5b83a2225780d3
shai-benari <s.benari1962@gmail.com>
2023-11-20 22:05:47 +0200

    acivity start ts

---
+++ tracker.ts
@@ -0,0 +1,14 @@
+
+
+type Activity = {
+    namet: string,
+    activity: string,
+    startTime: number,
+    endTime: number,
+};
+
+type activityArray = Activity[];
+
+const activitys: activityArray = [];
+
+
